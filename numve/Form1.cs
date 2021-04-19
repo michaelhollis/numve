bb3a693c94763dad6306d5bed30848db93aca6ba
Michael Hollis <RC0042216@student.riversidecollege.ac.uk>
2021-04-19 15:48:04 +0100

    yerrrs

--- numve/Form1.cs
+++ numve/Form1.cs
@@ -37,6 +37,36 @@ namespace numve
             richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
             richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
 
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + "is not an interger");
+            }
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + "is not an interger");
+                int i2=0
+            }
+
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+               
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
