c627dc248dbcae4c8552684cbd27b32f9386ebc1
Hollis <RC0042216@student.riversidecollege.ac.uk>
2021-04-19 15:24:03 +0100

    Add project files.

---
+++ numve/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numve
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double d1=5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3")+ "\n");
+            richTextBox1.AppendText(d1.ToString("N0")+ "\n");
+            richTextBox1.AppendText(d1.ToString("0")+ "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0")+ "\n");
+            richTextBox1.AppendText(d1.ToString("0.00e00")+ "\n");
+
+            double pi = 3.141592653589793;
+         double c = 299792458;
+
+            richTextBox1.AppendText(pi.ToString("0.00e00") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00e00") + "\n");
+            richTextBox1.AppendText(pi.ToString("0.00e00") + "\n");
+
+            richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
+            richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
+            richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
+            richTextBox1.AppendText(c .ToString("0.00e00") + "\n");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
